5d71160774eb3940d365871b61bdd3d908167759
princeantwi2k <antwi2k@gmail.com>
2021-11-23 16:31:06 +0800

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
 import './App.css';
-import About from './Component/About us/About';
+
 import Caro from './Component/Caro/Caro';
 import Footer from './Component/Footer/Footer';
 import Home from './Component/Home/Home';
 import Navbar from './Component/Navbar';
-import {BrowserRouter as Router,Switch,Route, BrowserRouter} from 'react-router-dom';
+
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
      <Router >
       <Navbar />
-      <Home />
-      <Caro />
+     <Home />
+     <Caro />
       <Footer />
+
       </Router>
 
    
