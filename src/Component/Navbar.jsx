8c00343d7f3dd4a45711b9ce49628c48b2b4cd9d
princeantwi2k <antwi2k@gmail.com>
2021-11-23 16:32:03 +0800

    added Navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./Nav.css";
 import pic1 from "../images/r.png";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-default navbar-jo" sm={6} md={12}>
+    <nav class="navbar navbar-default navbar-jo fixed-top" sm={6} md={12}>
       <div class="navbar-header">
         <img src={pic1} alt="pic" className="img" />
       </div>
 
       <ul>
-        <li class="active">Home</li>
-        <li>About us</li>
-        <li>Service</li>
-        <li>Contact</li>
+        <li>
+          <Link to="/" class="active" id="day">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" class="active">
+            About us
+          </Link>
+        </li>
       </ul>
     </nav>
   );
